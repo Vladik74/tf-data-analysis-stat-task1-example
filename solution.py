c5e0a507dccf41864d40b72d09ad941d074a153d
Vladik74 <72654185+Vladik74@users.noreply.github.com>
2023-04-07 00:50:03 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,11 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import gamma
 
-chat_id = 496613075 # Ваш chat ID, не меняйте название переменной
+chat_id = 773978697 # Ваш chat ID, не меняйте название переменной
 
-def solution(p: float, x: np.array):
-    alpha = 1 - p
-    k = 2/(56*56)
-    q1 = gamma(len(x), 1).ppf((1-alpha)/2)
-    q2 = gamma(len(x), 1).ppf((1+alpha)/2)
-    sum = np.sum(x)
-    return (k*((q1+sum)/2)-1/2, k*((q2+sum)/2)-1/2)
+def solution(x: np.array) -> float:
+    mu = -27+np.exp(1)
+    var = (np.exp(2)-54)*mu**2
+    a = 10/(x.mean()**2)
+    return a
