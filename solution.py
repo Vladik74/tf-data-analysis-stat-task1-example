99f024f73c76a5d37bc09412e5da7f6c94be7c26
Vladik74 <72654185+Vladik74@users.noreply.github.com>
2023-04-07 23:43:12 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
 
-
-chat_id = 496613075 # Ваш chat ID, не меняйте название переменной
+chat_id = 674922610 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    mu = -27+np.exp(1)
-    var = (np.exp(2)-54)*mu**2
-    a = 10/(x.mean()**2)
-    return a
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    return x.mean() / 26
